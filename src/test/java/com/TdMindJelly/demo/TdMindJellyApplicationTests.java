2a64875ea2defa94afb586fde517061da0b77655
jinhyeok <wlsgur348@gmail.com>
2025-01-08 10:05:25 +0900

    [FIX] Test 코드 수정 및 필요없는 gradle 삭제

--- src/test/java/com/TdMindJelly/demo/TdMindJellyApplicationTests.java
+++ src/test/java/com/TdMindJelly/demo/TdMindJellyApplicationTests.java
@@ -2,8 +2,9 @@ package com.TdMindJelly.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 
-@SpringBootTest
+@SpringBootTest(classes = TdMindJellyApplicationTests.class)
 class TdMindJellyApplicationTests {
 
 	@Test
